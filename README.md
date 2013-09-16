2f1ecd6d42cd599a5f0f360a4792859a2e349bac
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-09-16 11:33:28 +0100

    user and password now obtained from env with new name
    Also, readme file created and contains basic information for running
    the tool

---
+++ README.md
@@ -0,0 +1,17 @@
+# VCloud Walker
+
+We used thor to build a command line tool.
+
+to get list of commands: `bundle exec thor list`
+
+then you can issue a command such as walk. 
+
+    `bundle exec thor walk:vdcs <ORG-ID>`
+    `bundle exec thor walk:catalogs <ORG-ID>`
+    `bundle exec thor walk:networks <ORG-ID>`
+
+you will also need your user details in your environment:
+
+    `API_USERNAME`
+    `API_PASSWORD`
+
--- lib/vcloud_session.rb
+++ lib/vcloud_session.rb
@@ -1,14 +1,14 @@
 class VcloudSession
 
-  SKYSCAPE_API = 'api.vcd.portal.skyscapecloud.com'
+  API_URL = 'api.vcd.portal.skyscapecloud.com'
   API_VERSION = '5.1'
   TIMEOUT = 200
 
   def self.instance
     @@session ||= Fog::Compute::VcloudDirector.new(
-        :vcloud_director_host => SKYSCAPE_API,
-        :vcloud_director_username => '260.3.4edba2@4-3-37-b5567e',
-        :vcloud_director_password => ENV['SKYSCAPE_PASSWORD'],
+        :vcloud_director_host => API_URL,
+        :vcloud_director_username => ENV['API_USERNAME'],
+        :vcloud_director_password => ENV['API_PASSWORD'],
         :vcloud_director_api_version => API_VERSION,
         :connection_options => {:omit_default_port => true, :connect_timeout => TIMEOUT, :read_timeout => TIMEOUT})
   end
