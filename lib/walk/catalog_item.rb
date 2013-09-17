0f38229921baedaa61039ee1e03d4dba26e39c6a
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2013-09-17 20:31:37 +0100

    [#56345268] showing vapp_temp_id in summary

--- lib/walk/catalog_item.rb
+++ lib/walk/catalog_item.rb
@@ -1,11 +1,12 @@
 module Walk
   class CatalogItem < Entity
-    attr_accessor :id, :name, :description
+    attr_accessor :id, :name, :description, :vapp_template_id
 
     def initialize item
       self.id = item.id
       self.name = item.name
       self.description = item.description
+      self.vapp_template_id = item.vapp_template_id
     end
   end
 
