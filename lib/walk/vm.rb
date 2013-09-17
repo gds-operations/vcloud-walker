0157e12edd160d18bf1a3611b31a8e8cefe48b47
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2013-09-17 20:30:25 +0100

    [#56345268] added vm disk info

--- lib/walk/vm.rb
+++ lib/walk/vm.rb
@@ -8,7 +8,7 @@ module Walk
   end
 
   class Vm < Entity
-    attr_accessor :id, :status, :ip_address, :cpu, :memory, :operating_system
+    attr_accessor :id, :status, :ip_address, :cpu, :memory, :operating_system, :disks
     
     def initialize vm
       self.id = vm.id
@@ -17,24 +17,8 @@ module Walk
       self.cpu = vm.cpu
       self.memory = vm.memory
       self.operating_system = vm.operating_system
-      #self.disks = disk_summary(vm.disks.all(false))
+      self.disks = vm.hard_disks
     end
 
-    private
-    # fails to load disk summary due to huge object size
-    #def disk_summary disks
-    #  disks.collect do |disk|
-    #    {
-    #        address: disk.address,
-    #        description: disk.description,
-    #        name: disk.name,
-    #        capacity: disk.capacity,
-    #        resource_sub_type: dsik.resource_sub_type,
-    #        resource_type: disk.resource_type,
-    #        address_on_parent: disk.address_on_parent,
-    #        parent: disk.parent
-    #    }
-    #  end
-    #end
   end
 end
