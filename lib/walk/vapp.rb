6689426b343462e07dda09af4c297e9eaf10fb4a
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-09-18 16:23:02 +0100

    fix ability to walk one or more network configurations
    It seems that the API might return a list or a single item
    and we need to be able to deal with both (ideally the fog
    code would deal with this but it doesn't at the minute)
    
    There is also a fix here for a lack of a parent network

--- lib/walk/vapp.rb
+++ lib/walk/vapp.rb
@@ -11,16 +11,18 @@ module Walk
     end
 
     private
-    def extract_network_config network_config
-      {
-          network_name: network_config[:networkName],
-          is_deployed: network_config[:IsDeployed],
-          description: network_config[:Description],
-          config: {
-              ipscopes: network_config[:Configuration][:IpScopes]
-          },
-          parent_network: network_config[:Configuration][:ParentNetwork][:name]
-      }
+    def extract_network_config network_configs
+      (network_configs.is_a?(Hash)? [network_configs] : network_configs).collect do |network_config|
+        {
+            network_name: network_config[:networkName],
+            is_deployed: network_config[:IsDeployed],
+            description: network_config[:Description],
+            config: {
+                ipscopes: network_config[:Configuration][:IpScopes]
+            },
+            parent_network: network_config[:Configuration][:ParentNetwork]? network_config[:Configuration][:ParentNetwork][:name]: nil
+        }
+      end
     end
 
   end
