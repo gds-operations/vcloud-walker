aefa16b607e357c6dd543e2106fd00ddf0f924e6
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2013-09-19 14:10:30 +0100

    spec for describing catalogs
    adding tests for descibing catalogs and items within it.

---
+++ spec/walk/catalogs_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../spec_helper'
+require 'rspec/mocks'
+
+describe Walk::Catalogs do
+  let(:org) { double(:fog_org, :id => '4-3-51-7942a4') }
+  let(:organizations) { double(:get_by_name => org) }
+  let(:session) { double(:fog_api_session, :organizations => organizations) }
+
+  it "should walk catalogs within given org" do
+    Fog::Compute::VcloudDirector.should_receive(:new).and_return(session)
+
+    mock_item = mock(:catalog_item,
+                     :id => "12345",
+                     :name => 'ubuntu 11.04',
+                     :description => 'image for ubuntu 11.04',
+                     :vapp_template_id => 'vapp-template-01')
+    mock_catalog = mock(:catalog,
+                        :id => 'catalog_id_1',
+                        :name => 'Default catalog',
+                        :description => 'default catalog for infrastructure',
+                        :catalog_items => mock(:catalog_items, :all => [mock_item]))
+
+    org.should_receive(:catalogs).and_return(mock(:all => [mock_catalog]))
+
+    catalog_summary = Walk::Catalogs.new('4-3-51-7942a4').to_summary
+    catalog_summary.count.should == 1
+    catalog_summary.first[:items].count.should == 1
+    catalog_summary.should == [{
+                                   :id => "catalog_id_1",
+                                   :name => "Default catalog",
+                                   :description => "default catalog for infrastructure",
+                                   :items => [
+                                       {:id => "12345", :name => "ubuntu 11.04", :description => "image for ubuntu 11.04", :vapp_template_id => "vapp-template-01"}
+                                   ]
+                               }]
+  end
+
+end
