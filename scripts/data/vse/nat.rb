086aa80713213f53c5568274b46e74e5af2e94ea
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2013-10-08 18:09:27 +0100

    moving walker-ci to new vcloud_ci organization
    Changed the scripts to create walker ci in new org. Also updated the
    test json to use new data

--- scripts/data/vse/nat.rb
+++ scripts/data/vse/nat.rb
@@ -1,12 +1,12 @@
 nat do
   snat :desc => 'Outbound',
     :interface => 'walker_ci_vse',
-    :original => { :ip => '192.168.2.2' },
-    :translated => { :ip => '37.26.89.189' }
+    :original => { :ip => '192.168.254.100' },
+    :translated => { :ip => '37.26.90.84' }
 
   # Admin, deployment, and rsync.
   dnat :desc => 'SSH mirror0',
     :interface => 'walker_ci_vse',
-    :original => { :ip => '37.26.89.189', :port => 22, },
-    :translated => { :ip => '192.168.2.2', :port => 22, }
+    :original => { :ip => '37.26.90.84', :port => 22, },
+    :translated => { :ip => '192.168.254.100', :port => 22, }
 end
