ae6d3b50e04ed5c295ff810c5373bf05450078f1
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2013-10-10 14:29:03 +0100

    fixing ci power switch script

--- scripts/switch_power_for_walker_ci.rb
+++ scripts/switch_power_for_walker_ci.rb
@@ -2,13 +2,13 @@ require File.expand_path("../../lib/walk/walk.rb", __FILE__)
 
 usage = 'bundle exec ruby script/switch_power_for_walker_ci.rb (on/off)'
 unless ARGV.count == 1 && %w(on off).include?(ARGV[0])
-  p usage;
+  p usage
   raise 'Invalid Arguments'
 end
 
 power_mode = ARGV[0]
 
-org = Walk::Organization.get_by_id('4-3-59-7cb276')
+org = FogInterface.get_org
 vdc = org.vdcs.get_by_name('vCloud CI (IL2-DEVTEST-BASIC)')
 vapp = vdc.vapps.get_by_name 'vcloud-walker-contract-testing-vapp'
 response = vapp.send("power_#{power_mode}")
