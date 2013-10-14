7238149a8140b951c1770df544134ebb31095c56
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-10-14 17:16:49 +0100

    use .fog file for fog parameters
    CI integration build now generates a fog file from env
    vars passed in from Jenkins

--- lib/vcloud_session.rb
+++ lib/vcloud_session.rb
@@ -1,15 +1,7 @@
 class VcloudSession
 
-  API_URL = 'api.vcd.portal.skyscapecloud.com'
-
   def self.instance
-    raise ArgumentError.new('API credentials not found !') unless ENV['API_USERNAME'] && ENV['API_PASSWORD']
-
-    Fog::Compute::VcloudDirector.new(
-      :vcloud_director_host => API_URL,
-      :vcloud_director_username => ENV['API_USERNAME'],
-      :vcloud_director_password => ENV['API_PASSWORD'],
-    )
+    Fog::Compute::VcloudDirector.new
   end
 
 end
