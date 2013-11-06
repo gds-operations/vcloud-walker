db40f3f5d449a2c66bb27a799c672c3cf7b3edb3
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-11-06 16:07:49 +0000

    add integration test for edge gateways

--- spec/integration/vcloud_walker_spec.rb
+++ spec/integration/vcloud_walker_spec.rb
@@ -4,6 +4,14 @@ require 'stringio'
 
 JsonSpec.directory = File.expand_path('../data/walker_ci', __FILE__)
 
+#######################################################################################
+#  The intention of these tests are to ensure we have confdence that our tooling will
+#   function when used, and as integration tests to cover the areas that get mocked
+#   and so to uncover assumptions on how we use the fog libraries.
+#  They are not there to test fog - that is the job of the fog tests, though
+#   this can be reconsidered if we get a of undetected bugs.
+#  Most if not all edge cases should be caught by the unit tests or the Fog tests.
+#
 
 describe Walk::Vdcs do
   context 'walk an organization' do
@@ -38,6 +46,16 @@ describe Walk::Vdcs do
 
       #comparing catalogs added by us, ignoring the skyscape public catalogs
       select_walker_ci_catalog(catalog_summary).to_json.should be_json_eql(expected_catalog_summary)
+    end
+
+    it "should integrate with fog to get edge gateway data" do
+
+      result = VcloudWalk.new.edgegateways.first.to_json
+
+      result.should have_json_path('Configuration/EdgeGatewayServiceConfiguration/FirewallService')
+      result.should have_json_path('Configuration/EdgeGatewayServiceConfiguration/NatService')
+      result.should have_json_path('Configuration/EdgeGatewayServiceConfiguration/LoadBalancerService')
+      result.should have_json_path('Configuration/GatewayInterfaces/GatewayInterface')
     end
 
     private
