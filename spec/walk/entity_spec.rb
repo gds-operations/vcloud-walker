8d9c2abd7ecf7c6eb1af5cf6c15502f50ad093a2
Anna Shipman <anna@annashipman.co.uk>
2013-11-28 15:26:42 +0000

    Add test for part of to_summary functionality
    Small test but I think it's worth a test for the functionality that translates to a string, removes the @ and then symbolizes again.

--- spec/walk/entity_spec.rb
+++ spec/walk/entity_spec.rb
@@ -41,6 +41,15 @@ module Vcloud
 
       test_class.to_summary.should == {:test_data => [{:name => "collection 1"}, {:name => "collection 2"}], :description => "test class desc"}
     end
+
+    it 'should summaries a class as a hash and remove @ from the symbol names' do
+      collection = [double(:name => 'collection 1'), double(:name => 'collection 2')]
+      test_class = Vcloud::Walker::Resource::TestClass.new(double(:description => 'test class desc', :collection => collection))
+      test_class.instance_variables.should eq([:@test_data, :@description])
+      test_summary = test_class.to_summary
+      test_summary.keys.should eq([:test_data, :description])
+    end
+
   end
 end
 
