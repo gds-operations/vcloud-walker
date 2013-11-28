09ba8c0009e232b6e4203ccb34c18aa027f09cf2
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2013-11-28 16:23:59 +0000

    parametrised script to power_on walker_ci
    this script will be used against carrenza and skyscape both

--- scripts/switch_power_for_walker_ci.rb
+++ scripts/switch_power_for_walker_ci.rb
@@ -9,8 +9,8 @@ end
 power_mode = ARGV[0]
 
 org = Vcloud::Walker::FogInterface.get_org
-vdc = org.vdcs.get_by_name('vCloud CI (IL2-DEVTEST-BASIC)')
-vapp = vdc.vapps.get_by_name 'vcloud-walker-contract-testing-vapp'
+vdc = org.vdcs.get_by_name(ENV['VDC_NAME'])
+vapp = vdc.vapps.get_by_name ENV['WALKER_CI_VAPP']
 response = vapp.send("power_#{power_mode}")
 
 response ? (puts "Turned #{power_mode} walker ci.") : (raise "unable to turn #{power_mode} walker ci")
