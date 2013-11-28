237ddf91896780ddd8b2883375d9f33750c9f735
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2013-11-28 15:46:07 +0000

    configuring vse using fog#edgegateway.

--- scripts/configure_walker_ci_vse.rb
+++ scripts/configure_walker_ci_vse.rb
@@ -1,38 +1,31 @@
-require 'thor'
-require 'vcloud_network_configurator'
+require 'fog'
 
-class ConfigureWalkerCiVse < Thor
-  map "-L" => :list
+edge_gateway_id = ARGV[0]
+raise "please provide edgegateway id. usage: bx ruby ./configure_walker_ci_vse.rb <edgegateway-id>" unless edge_gateway_id
 
-  API_URL = 'https://api.vcd.portal.skyscapecloud.com/api'
-  WALKER_ORG_ID = '4-3-59-7cb276'
-  EDGE_GATEWAY_UUID = '27805d5d-868b-4678-b30b-11e14ad34eca'
+lb_config = {
+    :IsEnabled => "true",
+    :Pool => [],
+    :VirtualServer => []
+}
 
-  desc 'configures the walker CI VSE', 'configures lb, nat and firewall rules'
+configuration = {
+    :FirewallService =>
+        {
+            :IsEnabled => true,
+            :DefaultAction => 'allow',
+            :LogDefaultAction => false,
+            :FirewallRule => []
+        },
+    :LoadBalancerService => lb_config,
+    :NatService => {
+        :IsEnabled => true,
+        :nat_type => 'portForwarding',
+        :Policy => 'allowTraffic',
+        :NatRule => []
+    }
+}
 
-  def configure username, password
-    raise "API credentials not provided" unless username && password
-
-    rules_dir = File.expand_path(File.join(File.dirname(__FILE__), 'data/vse'))
-    common_args = ['-u', username,
-                   '-p', password,
-                   '-o', WALKER_ORG_ID,
-                   '-U', EDGE_GATEWAY_UUID,
-                   '-i', "#{rules_dir}/interfaces.yaml",
-    ]
-
-    %w{firewall nat lb}.each do |component|
-      puts "Executing component: #{component}"
-      args = common_args + ['-c', component,
-                            '-r', "#{rules_dir}/#{component}.rb",
-                            API_URL]
-      puts VcloudNetworkConfigurator.new(args).execute
-    end
-  end
-
-end
-
-ConfigureWalkerCiVse.new.configure(ARGV[0], ARGV[1])
-
-
-
+vcloud = Fog::Compute::VcloudDirector.new
+task = vcloud.post_configure_edge_gateway_services edge_gateway_id, configuration
+vcloud.process_task(task.body)
