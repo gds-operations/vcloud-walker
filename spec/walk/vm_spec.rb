73464e9d235be114a5efd2ba4274de04f14c48d5
Anna Shipman <anna@annashipman.co.uk>
2013-12-03 10:18:42 +0000

    Add storage profile to test VM
    Also make test names obviously testing. There's no information leakage here but updating it so that a casual observer doesn't think there might be.

--- spec/walk/vm_spec.rb
+++ spec/walk/vm_spec.rb
@@ -7,8 +7,8 @@ describe Vcloud::Walker::Resource::Vm do
       fog_vm = {
           :deployed => "true",
           :status => "8",
-          :name => "ubuntu-precise-201310041515",
-          :id => "urn:vcloud:vm:c31ecfcf-5cb0-417c-958c-0d0180cf7e2a",
+          :name => "ubuntu-testing-template",
+          :id => "urn:vcloud:vm:aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee",
           :"ovf:VirtualHardwareSection" =>
               {:"ovf:Info" => "Virtual hardware requirements",
                :"ovf:System" => {:"vssd:ElementName" => "Virtual Hardware Family", :"vssd:VirtualSystemType" => "vmx-08"},
@@ -39,7 +39,13 @@ describe Vcloud::Walker::Resource::Vm do
                           }
                       ]
               },
-          :RuntimeInfoSection => {:VMWareTools => {:version => "2147483647"}}
+          :RuntimeInfoSection => {:VMWareTools => {:version => "2147483647"}},
+          :StorageProfile=>
+              {
+                  :type=>"application/vnd.vmware.vcloud.vdcStorageProfile+xml",
+                  :name=>"TEST-STORAGE-PROFILE",
+                  :href=>"https://api.vcd.portal.skyscapecloud.com/api/vdcStorageProfile/00000000-aaaa-bbbb-aaaa-000000000000"
+              }
       }
 
       @vm_summary = Vcloud::Walker::Resource::Vm.new(fog_vm)
