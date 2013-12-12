c6242a264730a289ff92ab2c388dd15b4c217199
Anna Shipman <anna@annashipman.co.uk>
2013-12-12 10:29:35 +0000

    Add all existing thor functionality to methadone
    This is WIP, e.g. there is no test for the new method I've added in walker.rb; the error message is not informative, etc. Am committing this so that rest of team can see where I am going with this.

--- lib/vcloud/walker.rb
+++ lib/vcloud/walker.rb
@@ -5,3 +5,26 @@ require 'fog'
 require 'vcloud/walker/vcloud_session'
 require 'vcloud/walker/fog_interface'
 require 'vcloud/walker/resource'
+
+module Vcloud
+  module Walker
+
+    def self.walk(resource_to_walk)
+      print JSON.pretty_generate case resource_to_walk
+        when 'catalogs'
+          Vcloud::Walker::Resource::Organization.catalogs
+        when 'vdcs'
+          Vcloud::Walker::Resource::Organization.vdcs
+        when 'networks'
+          Vcloud::Walker::Resource::Organization.networks
+        when 'edgegateways'
+          Vcloud::Walker::Resource::Organization.edgegateways
+        when 'organization'
+          Vcloud::Walker::Resource::Organization.all
+        else
+          puts 'Please enter a valid resource; one of x, y, z...'
+        end
+    end
+
+  end
+end
