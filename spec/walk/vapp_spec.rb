d555e62bd1eec6764007a986c6b37f28b38bfb41
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2013-12-18 10:23:23 +0000

    spec - vapp should have id in correct format

---
+++ spec/walk/vapp_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Vcloud::Walker::Resource::VApp do
+
+  context 'populate summary vapp model' do
+    before(:all) do
+      fog_vapp = Fog::ServiceLayerStub.vapp_body
+
+      @vm_summary = Vcloud::Walker::Resource::VApp.new(fog_vapp)
+    end
+
+    it 'should report id from the href' do
+      @vm_summary.id.should == 'vapp-aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee'
+    end
+  end
+end
