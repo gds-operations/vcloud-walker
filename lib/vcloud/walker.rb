6c3c7990517f5b646a3ea2f42bec376dba6a67a9
Anna Shipman <anna@annashipman.co.uk>
2013-12-22 11:17:29 +0000

    Replace case statement
    Thanks @snehaso!

--- lib/vcloud/walker.rb
+++ lib/vcloud/walker.rb
@@ -12,18 +12,7 @@ module Vcloud
       valid_options = ['catalogs', 'vdcs', 'networks',
                         'edgegateways', 'organization']
       if valid_options.include? resource_to_walk
-        case resource_to_walk
-          when 'catalogs'
-            Vcloud::Walker::Resource::Organization.catalogs
-          when 'vdcs'
-            Vcloud::Walker::Resource::Organization.vdcs
-          when 'networks'
-            Vcloud::Walker::Resource::Organization.networks
-          when 'edgegateways'
-            Vcloud::Walker::Resource::Organization.edgegateways
-          when 'organization'
-            Vcloud::Walker::Resource::Organization.organization
-        end
+        Vcloud::Walker::Resource::Organization.send(resource_to_walk)
       else
          puts "Possible options are '#{valid_options.join("','")}'."
       end
