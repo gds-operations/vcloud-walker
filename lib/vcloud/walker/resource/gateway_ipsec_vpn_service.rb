1b6167544784908284c10129d4f5cee315e599d8
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2014-02-05 16:49:18 +0000

    should walk vpn service with multiple tunnels

--- lib/vcloud/walker/resource/gateway_ipsec_vpn_service.rb
+++ lib/vcloud/walker/resource/gateway_ipsec_vpn_service.rb
@@ -2,38 +2,40 @@ module Vcloud
   module Walker
     module Resource
       class GatewayIpsecVpnService < Entity
-        attr_reader :IsEnabled, :Tunnel
+        attr_reader :IsEnabled, :Tunnels
 
         def initialize fog_vpn_service
           @IsEnabled = fog_vpn_service[:IsEnabled]
-          fog_vpn_tunnel = fog_vpn_service[:Tunnel]
-
-          @Tunnel = populate_tunnel_info(fog_vpn_tunnel)
+          if fog_vpn_service[:Tunnel]
+            fog_vpn_tunnels = [fog_vpn_service[:Tunnel]].flatten
+            @Tunnels = populate_tunnel_info(fog_vpn_tunnels)
+          end
         end
 
         private
-        def populate_tunnel_info(fog_vpn_tunnel)
-          return unless fog_vpn_tunnel
+        def populate_tunnel_info(fog_vpn_tunnels)
+          return if fog_vpn_tunnels.empty?
+          fog_vpn_tunnels.collect do |fog_vpn_tunnel|
+            @tunnel = {
+              :Name => fog_vpn_tunnel[:Name],
+              :Description => fog_vpn_tunnel[:Description],
+              :ThirdPartyPeerId => fog_vpn_tunnel[:IpsecVpnThirdPartyPeer][:PeerId],
+              :PeerId => fog_vpn_tunnel[:PeerId],
+              :LocalId => fog_vpn_tunnel[:LocalId],
+              :PeerIpAddress => fog_vpn_tunnel[:PeerIpAddress],
+              :LocalIpAddress => fog_vpn_tunnel[:LocalIpAddress],
+              :PeerSubnet => fog_vpn_tunnel[:PeerSubnet],
+              :LocalSubnet => fog_vpn_tunnel[:LocalSubnet],
+              :Mtu => fog_vpn_tunnel[:Mtu],
+              :IsEnabled => fog_vpn_tunnel[:IsEnabled],
+              :IsOperational => fog_vpn_tunnel[:IsOperational]
+            }
 
-          @tunnel = {
-            :Name => fog_vpn_tunnel[:Name],
-            :Description => fog_vpn_tunnel[:Description],
-            :ThirdPartyPeerId => fog_vpn_tunnel[:IpsecVpnThirdPartyPeer][:PeerId],
-            :PeerId => fog_vpn_tunnel[:PeerId],
-            :LocalId => fog_vpn_tunnel[:LocalId],
-            :PeerIpAddress => fog_vpn_tunnel[:PeerIpAddress],
-            :LocalIpAddress => fog_vpn_tunnel[:LocalIpAddress],
-            :PeerSubnet => fog_vpn_tunnel[:PeerSubnet],
-            :LocalSubnet => fog_vpn_tunnel[:LocalSubnet],
-            :Mtu => fog_vpn_tunnel[:Mtu],
-            :IsEnabled => fog_vpn_tunnel[:IsEnabled],
-            :IsOperational => fog_vpn_tunnel[:IsOperational]
-          }
-
-          @tunnel[:SharedSecret] = "*" * 65 if fog_vpn_tunnel[:SharedSecret]
-          @tunnel[:SharedSecretEncrypted] = "******" if fog_vpn_tunnel[:SharedSecretEncrypted]
-          @tunnel[:EncryptionProtocol] = "******" if fog_vpn_tunnel[:EncryptionProtocol]
-          @tunnel
+            @tunnel[:SharedSecret] = "*" * 65 if fog_vpn_tunnel[:SharedSecret]
+            @tunnel[:SharedSecretEncrypted] = "******" if fog_vpn_tunnel[:SharedSecretEncrypted]
+            @tunnel[:EncryptionProtocol] = "******" if fog_vpn_tunnel[:EncryptionProtocol]
+            @tunnel
+          end
         end
 
 
--- spec/walk/gateway_ipsec_vpn_service_spec.rb
+++ spec/walk/gateway_ipsec_vpn_service_spec.rb
@@ -1,86 +1,157 @@
 require 'spec_helper'
 
 describe Vcloud::Walker::Resource::GatewayIpsecVpnService do
-  before(:all) do
+  context "vpn service with single tunnel" do
+    before(:all) do
+      fog_vpn_service = {
+        IsEnabled: true,
+        Tunnel: {
+          Name: "ss_one",
+          Description: "desc one",
+          IpsecVpnThirdPartyPeer: {
+            PeerId: "1"
+          },
+          PeerIpAddress: "8.8.8.8",
+          PeerId: "1",
+          LocalIpAddress: "192.0.2.1",
+          LocalId: "1",
+          LocalSubnet: {
+            Name: "Default",
+            Gateway: "192.168.0.1",
+            Netmask: "255.255.0.0"
+          },
+          PeerSubnet: {
+            Name: "8.8.8.0/8",
+            Gateway: "8.8.8.0",
+            Netmask: "255.0.0.0"
+          },
+          SharedSecret: "DoNotDiscloseDoNotDiscloseDoNotDiscloseDoNotDiscloseDoNotDisclose",
+          SharedSecretEncrypted: "false",
+          EncryptionProtocol: "AES256",
+          Mtu: "1500",
+          AdditionalDetails: {
+            shared_network: true
+          },
+          IsEnabled: "true",
+          IsOperational: "false"
+        }
+      }
+
+      @gateway_vpn_service = Vcloud::Walker::Resource::GatewayIpsecVpnService.new(fog_vpn_service)
+    end
+    it "should report status of vpn service" do
+      expect(@gateway_vpn_service.IsEnabled).to eq(true)
+      expect(@gateway_vpn_service.Tunnels).not_to be_empty
+    end
+
+    context "report tunnel info" do
+      before(:all) do
+        @tunnel = @gateway_vpn_service.Tunnels.first
+      end
+
+      it "with peer details" do
+        expect(@tunnel[:PeerId]).to eq('1')
+        expect(@tunnel[:PeerIpAddress]).to eq('8.8.8.8')
+        expect(@tunnel[:PeerSubnet]).to eq({
+                                             Name: "8.8.8.0/8",
+                                             Gateway: "8.8.8.0",
+                                             Netmask: "255.0.0.0"
+                                           })
+      end
+
+      it "with local network details" do
+        expect(@tunnel[:LocalId]).to eq('1')
+        expect(@tunnel[:LocalIpAddress]).to eq('192.0.2.1')
+        expect(@tunnel[:LocalSubnet]).to eq({
+                                              Name: "Default",
+                                              Gateway: "192.168.0.1",
+                                              Netmask: "255.255.0.0"
+                                            })
+      end
+
+      it "with maximum transmission unit" do
+        expect(@tunnel[:Mtu]).to eq('1500')
+      end
+
+      it "with masked vpn shared secret information" do
+        expect(@tunnel[:SharedSecret]).to eq("*" * 65)
+        expect(@tunnel[:SharedSecretEncrypted]).to eq("******")
+        expect(@tunnel[:EncryptionProtocol]).to eq("******")
+      end
+
+      it "should skip any addditional vpn details provided by fog" do
+        expect(@tunnel[:AdditionalDetails]).to be_nil
+      end
+    end
+
+  end
+
+  it "should report vpn service with multiple tunnels" do
     fog_vpn_service = {
       IsEnabled: true,
-      Tunnel: {
-        Name: "ss_one",
-        Description: "desc one",
-        IpsecVpnThirdPartyPeer: {
-          PeerId: "1"
+      Tunnel: [
+        {
+          Name: "remove vpn 1",
+          Description: "describe me",
+          IpsecVpnThirdPartyPeer: {
+            PeerId: "1"
+          },
+          PeerIpAddress: "8.8.8.8",
+          PeerId: "1",
+          LocalIpAddress: "192.0.2.1",
+          LocalId: "1",
+          LocalSubnet: {
+            Name: "Default",
+            Gateway: "192.168.0.1",
+            Netmask: "255.255.0.0"
+          },
+          PeerSubnet: {
+            Name: "8.8.8.0/8",
+            Gateway: "8.8.8.0",
+            Netmask: "255.0.0.0"
+          },
+          SharedSecret: "DoNotDiscloseDoNotDiscloseDoNotDiscloseDoNotDiscloseDoNotDisclose",
+          SharedSecretEncrypted: "false",
+          EncryptionProtocol: "AES256",
+          Mtu: "1500",
+          AdditionalDetails: {
+            shared_network: true
+          },
+          IsEnabled: "true",
+          IsOperational: "false"
         },
-        PeerIpAddress: "8.8.8.8",
-        PeerId: "1",
-        LocalIpAddress: "192.0.2.1",
-        LocalId: "1",
-        LocalSubnet: {
-          Name: "Default",
-          Gateway: "192.168.0.1",
-          Netmask: "255.255.0.0"
-        },
-        PeerSubnet: {
-          Name: "8.8.8.0/8",
-          Gateway: "8.8.8.0",
-          Netmask: "255.0.0.0"
-        },
-        SharedSecret: "DoNotDiscloseDoNotDiscloseDoNotDiscloseDoNotDiscloseDoNotDisclose",
-        SharedSecretEncrypted: "false",
-        EncryptionProtocol: "AES256",
-        Mtu: "1500",
-        AdditionalDetails: {
-          shared_network: true
-        },
-        IsEnabled: "true",
-        IsOperational: "false"
-      }
+        {
+          Name: "remote vpn 2",
+          Description: "describe me",
+          IpsecVpnThirdPartyPeer: {
+            PeerId: "2"
+          },
+          PeerIpAddress: "8.8.4.4",
+          PeerId: "2",
+          LocalIpAddress: "172.26.2.3",
+          LocalId: "2",
+          LocalSubnet: {
+            Name: "Default",
+            Gateway: "192.168.0.1",
+            Netmask: "255.255.0.0"
+          },
+          PeerSubnet: {
+            Name: "8.8.4.4/8",
+            Gateway: "8.8.4.4",
+            Netmask: "255.0.0.0"
+          },
+          SharedSecret: "PrivyPrivyPrivyPrivyPrivyPrivyPrivyPrivyPrivyPrivy",
+          SharedSecretEncrypted: "false",
+          EncryptionProtocol: "AES256",
+          Mtu: "1500",
+          IsEnabled: "true",
+          IsOperational: "false"
+        }
+      ]
     }
 
-    @gateway_vpn_service = Vcloud::Walker::Resource::GatewayIpsecVpnService.new(fog_vpn_service)
-  end
-  it "should report status of vpn service" do
-    expect(@gateway_vpn_service.IsEnabled).to eq(true)
-    expect(@gateway_vpn_service.Tunnel).not_to be_nil
-  end
-
-  context "report tunnel info" do
-    before(:all) do
-      @tunnel = @gateway_vpn_service.Tunnel
-    end
-
-    it "with peer details" do
-      expect(@tunnel[:PeerId]).to eq('1')
-      expect(@tunnel[:PeerIpAddress]).to eq('8.8.8.8')
-      expect(@tunnel[:PeerSubnet]).to eq({
-                                           Name: "8.8.8.0/8",
-                                           Gateway: "8.8.8.0",
-                                           Netmask: "255.0.0.0"
-                                         })
-    end
-
-    it "with local network details" do
-      expect(@tunnel[:LocalId]).to eq('1')
-      expect(@tunnel[:LocalIpAddress]).to eq('192.0.2.1')
-      expect(@tunnel[:LocalSubnet]).to eq({
-                                            Name: "Default",
-                                            Gateway: "192.168.0.1",
-                                            Netmask: "255.255.0.0"
-                                          })
-    end
-
-    it "with maximum transmission unit" do
-      expect(@tunnel[:Mtu]).to eq('1500')
-    end
-
-    it "with masked vpn shared secret information" do
-      expect(@tunnel[:SharedSecret]).to eq("*" * 65)
-      expect(@tunnel[:SharedSecretEncrypted]).to eq("******")
-      expect(@tunnel[:EncryptionProtocol]).to eq("******")
-    end
-
-    it "should skip any addditional vpn details provided by fog" do
-       expect(@tunnel[:AdditionalDetails]).to be_nil
-    end
+    gateway_vpn_service = Vcloud::Walker::Resource::GatewayIpsecVpnService.new(fog_vpn_service)
+    expect(gateway_vpn_service.Tunnels.count).to eq(2)
   end
 
 end
