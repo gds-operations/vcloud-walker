962c1215b0f83794650e3920d8e1fe5af0fb310c
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2014-02-05 13:01:35 +0000

    nested entities should be summarized by walker

--- lib/vcloud/walker/resource/entity.rb
+++ lib/vcloud/walker/resource/entity.rb
@@ -7,7 +7,7 @@ module Vcloud
           h= {}
           instance_variables.each { |atr|
             atr_value = self.instance_variable_get(atr)
-            if atr_value.is_a?(Vcloud::Walker::Resource::Collection)
+            if atr_value.is_a?(Vcloud::Walker::Resource::Collection) || atr_value.is_a?(Entity)
               h[atr.to_s.delete("@").to_sym] = atr_value.to_summary
             else
               h[atr.to_s.delete("@").to_sym] = atr_value
