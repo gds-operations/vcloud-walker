c3822ef96f034e78208b31ff0b58244aad89a78f
Dan Carley <dan.carley@gmail.com>
2014-07-30 08:08:47 +0100

    Consolidate jenkins test scripts
    There was a lot of duplication between these scripts. Whereas the only
    differences were us not doing the following in
    `jenkins_integration_tests.sh` which is run against Carrenza:
    
    - Not running unit tests. Which are fast and we wouldn't want to run the
      integration tests if they do fail.
    - Publishing the gem. Which I've left in the original `jenkins.sh`.
    
    We can later change the Carrenza job to run `jenkins_tests.sh` directly and
    delete `jenkins_integration_tests.sh`.

---
+++ jenkins_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+set -e
+
+rm -f Gemfile.lock
+git clean -fdx
+
+bundle install --path "${HOME}/bundles/${JOB_NAME}"
+bundle exec rake
+
+RUBYOPT="-r ./tools/fog_credentials" bundle exec rake integration
