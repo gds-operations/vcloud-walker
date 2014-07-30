7795596ab1364c93db74efe1ae09bae8e7589093
Dan Carley <dan.carley@gmail.com>
2014-07-30 08:16:55 +0100

    Use vcloud-login for integration tests
    Writes a temporary FOG_RC containing the API host and username which is used
    by both vcloud-login and the integration tests.

--- jenkins_tests.sh
+++ jenkins_tests.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -x
-set -e
+set -eu
+
+function cleanup {
+  rm $FOG_RC
+}
+
+# Override default of ~/.fog and delete afterwards.
+export FOG_RC=$(mktemp /tmp/vcloud_fog_rc.XXXXXXXXXX)
+trap cleanup EXIT
+
+cat <<EOF >${FOG_RC}
+${FOG_CREDENTIAL}:
+  vcloud_director_host: '${API_HOST}'
+  vcloud_director_username: '${API_USERNAME}'
+  vcloud_director_password: ''
+EOF
 
 rm -f Gemfile.lock
 git clean -fdx
@@ -7,4 +22,5 @@ git clean -fdx
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 bundle exec rake
 
-RUBYOPT="-r ./tools/fog_credentials" bundle exec rake integration
+eval $(printenv API_PASSWORD | bundle exec vcloud-login)
+bundle exec rake integration
